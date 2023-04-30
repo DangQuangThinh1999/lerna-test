6b2730c5c15e7bdd7d4bbc88d90a8ae8cb6446fe
DangQuangThinh1999 <quangthinh123qb@gmail.com>
2023-04-30 12:35:41 +0700

    update nx

--- packages/admin/next.config.js
+++ packages/admin/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/admin",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/admin",
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
