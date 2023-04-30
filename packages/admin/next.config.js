7835f22f2b85bf2761e1ad800cc9cead5f38387e
DangQuangThinh1999 <quangthinh123qb@gmail.com>
2023-04-30 16:58:37 +0700

    config subdomain project admin

--- packages/admin/next.config.js
+++ packages/admin/next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/admin",
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/admin",
-        basePath: false,
-        permanent: false,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
